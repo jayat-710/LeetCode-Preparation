a8ccf1ac8975c961f23dc13172f09a4bd21c9b82
Jayat <jayat.19bce7604@vitap.ac.in>
2021-11-11 00:26:44 +0530

    Time: 0 ms (100.00%), Space: 36 MB (55.70%) - LeetHub

--- integer-replacement/integer-replacement.java
+++ integer-replacement/integer-replacement.java
@@ -1,10 +1,11 @@
 class Solution {
     public int integerReplacement(int n) {
-        
+         int res=0;
         if(n==2147483647){
-            return 32;
+            n=n-1;
+            //res++;
         }
-        int res=0;
+       
         while(n!=1){
             if(n%2==0){
                 n=n/2;
