b5a465e0763b42e6afa3e2879f69df503e72a6f1
Jayat <jayat.19bce7604@vitap.ac.in>
2021-11-22 01:41:37 +0530

    Time: 185 ms (23.17%), Space: 39.1 MB (29.31%) - LeetHub

--- burst-balloons/burst-balloons.java
+++ burst-balloons/burst-balloons.java
@@ -1,26 +1,41 @@
 class Solution {
+    static int dp[][]=new int[600][600];
     public int maxCoins(int[] nums) {
-        int n = nums.length;
-        int dp[][]= new int[n][n];
+       
+        //mcmm problem
         
-        //gap strategy
+        // i , j , k
+        //mene ek point liya k, mei yeh bolunga ki mera jo i...k ---- k...i usme mei yeh check karunga ki kaha k ko bithane se mujhe max milega
+        int n =nums.length;
         
-        for(int gap=0;gap<n;gap++){
-            for(int i=0,j=gap;j<n;i++,j++){
-                int max = Integer.MIN_VALUE;
-                for(int k=i;k<=j;k++){
-                    int left = k==i?0 : dp[i][k-1];
-                    int right= k==j?0 : dp[k+1][j];
-                    
-                    int val = (i==0?1:nums[i-1]) *nums[k]*(j==n-1?1:nums[j+1]);
-                    
-                    max = Math.max(max,val+left+right);
-                } dp[i][j]=max;
+        for(int i=0;i<=n+2;i++){
+            for(int j=0;j<=n+2;j++){
+                dp[i][j]=-1;
             }
+        }
+        return solve(-1,n,nums,n,dp);
+        
+        
+    }
+    
+    static int solve(int i,int j,int nums[],int n,int dp[][]){
+        
+        if(i+1==j)
+        return 0;
             
-           
+        int left= (i==-1)?1:nums[i];
+        int right = (j==n)?1:nums[j];
+        
+        int max =0;
+        
+        if(dp[i+1][j+1]!=-1){
+            return dp[i+1][j+1];
         }
         
-        return dp[0][n-1];
+        for(int k=i+1;k<j;k++){
+            max = Math.max(solve(i,k,nums,n,dp)+solve(k,j,nums,n,dp)+left*nums[k]*right,max);
+        }
+        
+        return dp[i+1][j+1]=max;
     }
 }
