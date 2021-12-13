ff226c0d7b65b2b2eeededcbaefa71c7b1cb846c
Jayat <jayat.19bce7604@vitap.ac.in>
2021-12-13 11:47:36 +0530

    Time: 1 ms (100.00%), Space: 40.2 MB (43.70%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -1,30 +1,14 @@
 class Solution {
-    static int dp[]=new int[100000];
     public boolean canJump(int[] nums) {
-        int n=nums.length;
-        int i=0;
-        Arrays.fill(dp,-1);
-        return solve(nums,i,n);
-    }
-    
-    static boolean solve(int nums[],int i,int n){
-       
-        if(i>=n-1)return true;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(max>=nums.length-1)return true;
+            
+            if(nums[i]==0 && max<=i)return false;
+            
+            max=Math.max(max,i+nums[i]);
+        }
         
-        if( nums[i]==0){
-            dp[i]=0;
-            return false;
-        }
-        if(dp[i]!=-1)return dp[i]==1;
-        boolean res=false;
-        for(int j=1;j<=nums[i];++j){
-           if(solve(nums,i+j,n)){
-               dp[i]=1;
-               res=true;
-               break;
-           }
-        }
-        dp[i]=0;
-        return res;
+        return false;
     }
 }
