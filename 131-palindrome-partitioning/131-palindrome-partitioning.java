72a69e93d9fae2fbdac3fff07e4773cef130aa0c
Jayat <jayat.19bce7604@vitap.ac.in>
2022-06-07 17:59:07 +0530

    Time: 30 ms (12.32%), Space: 194.1 MB (34.29%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,41 +1,40 @@
 class Solution {
-    static List<List<String>>ans;
+    static ArrayList<List<String>> ans ;
+    
     public List<List<String>> partition(String s) {
-       ans = new ArrayList<>();
-        // List<String> list = new ArrayList<>();
-        
-        helper(s,new ArrayList<>());
+        ans = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        solve(s,list);
         
         return ans;
     }
     
-    static void helper(String str,List<String> list){
-            if(str.length()==0){
-                ans.add(new ArrayList<>(list));
-                return ;
-            }
+    static void solve(String s,List<String> list){
         
-        for(int i=0;i<str.length();i++){
-            String sub=str.substring(0,i+1);
-            String ros=str.substring(i+1);
+        if(s.length()==0){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        
+        for(int k=0;k<s.length();k++){
+            String sub =s.substring(0,k+1);
+            String ros=s.substring(k+1);
             
             if(isPal(sub)){
                 list.add(sub);
-                helper(ros,list);
+                solve(ros,list);
                 list.remove(list.size()-1);
             }
         }
     }
     
     static boolean isPal(String str){
-        int i=0;int j=str.length()-1;
-        while(i<j){
+        int i=0; int j=str.length()-1;
+        while(i<=j){
             if(str.charAt(i)!=str.charAt(j))return false;
-            
-            i++;
-            j--;
+            i++;j--;
         }
+        
         return true;
-            
     }
 }
